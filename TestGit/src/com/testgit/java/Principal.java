7e7b652a446deb41743eb40c29c2ce5249100de1
Facundo <facundo@autoclerk.com>
2018-05-17 18:27:26 -0300

    MYH-1065 SEGUNDO COMMIT

--- TestGit/src/com/testgit/java/Principal.java
+++ TestGit/src/com/testgit/java/Principal.java
@@ -3,7 +3,8 @@ package com.testgit.java;
 public class Principal {
 
 	public static void main(String[] args) {
-		System.out.println(" HOLA MUNDO TODO BIEN ");
+		System.out.println(" **************************************************************************");		
+		System.out.println(" HOLA MUNDO TODO BIEN  ");
 		
 
 	}
