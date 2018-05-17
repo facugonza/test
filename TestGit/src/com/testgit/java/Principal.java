661e85dcc0b800aa4bfa4bd5f46962436257e68d
Facundo <facundo@autoclerk.com>
2018-05-17 19:13:21 -0300

    cuarto commit

--- TestGit/src/com/testgit/java/Principal.java
+++ TestGit/src/com/testgit/java/Principal.java
@@ -5,7 +5,8 @@ public class Principal {
 	public static void main(String[] args) {
 		System.out.println(" **************************************************************************");		
 		System.out.println(" HOLA MUNDO TODO BIEN      ---- ");
-		System.out.println("SALIDA *****************************************************");
+		System.out.println("SALIDA ***************************");
+		
 		
 
 	}
